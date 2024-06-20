3164efe89dc6da5595ad5e3e9cdde47c0daf1421
marshall-bits <kz_@hotmail.es>
2024-06-20 19:31:09 +0200

    add home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { SearchResourceComponent } from './search-resource/search-resource.component';
+import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
   { path: 'search', component: SearchResourceComponent },
+  { path: '', component: HomeComponent}
 ];
