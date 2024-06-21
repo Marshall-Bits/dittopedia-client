f1592cca9ed9c72003a1ef98c460ce71511f68d3
marshall-bits <kz_@hotmail.es>
2024-06-21 09:45:46 +0200

    add create resource component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { SearchResourceComponent } from './search-resource/search-resource.component';
 import { HomeComponent } from './home/home.component';
+import { CreateResourceComponent } from './create-resource/create-resource.component';
 export const routes: Routes = [
   { path: 'search', component: SearchResourceComponent },
+  { path: 'resource/create', component: CreateResourceComponent},
   { path: '', component: HomeComponent}
 ];
---
+++ src/app/create-resource/create-resource.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-resource',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-resource.component.html',
+  styleUrl: './create-resource.component.css'
+})
+export class CreateResourceComponent {
+
+}
