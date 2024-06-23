053a4c12637aceacd46e46bb0efe41d6f2622f0f
marshall-bits <kz_@hotmail.es>
2024-06-23 13:36:24 +0200

    update angular.json for public folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ import { NavbarComponent } from './navbar/navbar.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dittopedia-client';
+  title = 'dittopedia';
 }
