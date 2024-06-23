da10d27d2fb24b50eeeb3231b23c4937d6f37ab4
marshall-bits <kz_@hotmail.es>
2024-06-23 12:41:22 +0200

    update visibility of up-button

--- src/app/up-button/up-button.component.ts
+++ src/app/up-button/up-button.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-up-button',
   standalone: true,
   imports: [],
   templateUrl: './up-button.component.html',
-  styleUrl: './up-button.component.css'
+  styleUrl: './up-button.component.css',
 })
 export class UpButtonComponent {
+  isVisible = false;
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.isVisible = window.scrollY > 100;
+  }
+
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
